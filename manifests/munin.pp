c2738f74858eb304006bc7577d49a0b8ade9b1e2
mh <mh@immerda.ch>
2016-12-01 00:00:21 +0100

    add support for munin and varnish4

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -1,6 +1,6 @@
+# munin plugins for varnish
 class varnish::munin {
-  munin::plugin{
-    ['varnish_expunge',
+  $aspects = ['varnish_expunge',
     'varnish_transfer_rates',
     'varnish_objects',
     'varnish_uptime',
@@ -8,7 +8,21 @@ class varnish::munin {
     'varnish_memory_usage',
     'varnish_hit_rate',
     'varnish_threads',
-    'varnish_backend_traffic' ]:
-      ensure => "varnish_",
-  }   
+    'varnish_backend_traffic' ]
+
+  munin::plugin{$aspects: }
+  if versioncmp($operatingsystemmajrelease,'7') >= 0 {
+    Munin::Plugin[$aspects]{
+      ensure => 'varnish_',
+    }
+  } else {
+    munin::plugin::deploy{
+      'varnish4':
+        source   => 'varnish/munin/varnish4_',
+        register => false,
+    } -> Munin::Plugin[$aspects]{
+      ensure => 'varnish4_',
+      config => 'group varnish',
+    }
+  }
 }
